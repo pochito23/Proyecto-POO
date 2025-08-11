14ff3a569e4fe0e54b5756e224b288703f05bafd
mg729 <mariagallegos1404@gmail.com>
2025-08-11 01:25:32 -0600

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-import express,{ Express } from "express";
+import express, { Express } from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(cors()); //Cors
+
+app.get('/', (req, res) => {
+    res.send('API en funcionamiento');
+})
+
+app.listen(3107, () => {
+    console.log("Servidor funcionando en el puerto");
+});
